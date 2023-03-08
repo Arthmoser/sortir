a34893b909beae661c4e204699ae0d8bdd149c56
MaddyAlletru <maddy.alletru2022@campus-eni.fr>
2023-03-08 11:38:04 +0100

    LocationController modif

--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -16,32 +16,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class LocationController extends AbstractController
 {
     #[Route('/add', name: 'addLocation')]
-    public function addLocation(LocationRepository $locationRepository, Request $request): Response
+    public function addLocation(
+        LocationRepository $locationRepository,
+        Request $request): Response
     {
-        /**
-         * @var User $user
-         */
-        $user = $this->getUser();
 
         $location = new Location();
 
-        $locationForm = $this->createForm(LocationType::class,$location);
+        $locationForm = $this->createForm(LocationType::class, $location);
         $locationForm->handleRequest($request);
 
         if ($locationForm->isSubmitted() && $locationForm->isValid()) {
+
             $location
-                ->setCity("city")
-                ->setName("name")
-                ->setStreet("street")
-                ->setLongitude("longitude")
-                ->setLatitude("latitude");
+
+                ->setCity($locationForm->get('city')->getData());
+//                ->setName($locationForm->get('name')->getData())
+//                ->setStreet($locationForm->get('street')->getData())
+//                ->setLongitude($locationForm->get('longitude')->getData())
+//                ->setLatitude($locationForm->get('latitude')->getData());
 
             $locationRepository->save($location, true);
             $this->addFlash("success", "Lieu ajouté !");
 
-            return $this->redirectToRoute('main_home', ['id' => $location->getId()]);
-
+            return $this->redirectToRoute('main_home', ['id' => $location->getCity()->getId()]);
         }
+        dump($location);
+
             return $this->render('location.html.twig', [
                 'locationForm' => $locationForm->createView()
             ]);
--- src/Form/LocationType.php
+++ src/Form/LocationType.php
@@ -2,23 +2,46 @@
 
 namespace App\Form;
 
+use App\Entity\City;
 use App\Entity\Location;
+use App\Repository\CityRepository;
+use Doctrine\ORM\Mapping\Entity;
 use Faker\Provider\Text;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Choice;
 
 class LocationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
-            ->add('street', TextType::class)
-            ->add('latitude', TextType::class)
-            ->add('longitude', TextType::class)
-            ->add('city',TextType::class)
+            ->add('name', TextType::class, [
+                'label' => 'Nom du lieu'
+            ])
+            ->add('street', TextType::class, [
+                    'label' => 'Rue'
+                ])
+            ->add('latitude', TextType::class, [
+                    'label' => 'Latitude (optionnel)'
+            ])
+            ->add('longitude', TextType::class, [
+                'label' => 'Longitude (optionnel)'
+            ])
+            ->add('city',EntityType::class, [
+                'class' => City::class,
+                'choice_label' => 'name',
+                'label' => 'Ville',
+                'query_builder' => function(CityRepository $cityRepository){
+                $qb = $cityRepository->createQueryBuilder(("c"));
+                $qb->addOrderBy("c.name");
+                return $qb;
+                }
+            ])
         ;
     }
 
